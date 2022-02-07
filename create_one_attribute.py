c04813bdcb00e5e5b35d643c466d13f0840e3fd2
Husan <husanjalilov00777@gmail.com>
2022-02-07 09:11:39 +0500

    Task 01

--- create_one_attribute.py
+++ create_one_attribute.py
@@ -1,6 +1,7 @@
 #Create a "Person" class
 #Create an attribute "name" in the "Person" class
 class  Person:
-    def __init__(self,name):
+    def __init__(self,name,age):
         pass
         self.name=name
+        self.ege=ege
